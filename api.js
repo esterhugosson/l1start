f7f81bbe888bf88d15b2cfd2dda54ae0c825283f
Ester Hugosson <eh225bx@student.lnu.se>
2024-09-08 18:24:31 +0200

    Api file after example from documentation

--- api.js
+++ api.js
@@ -1,8 +1,30 @@
 // Provides object with nameday and name, for given name. empty array with no name day. 
+//Api https://nameday.abalin.net, for document and examples: https://nameday.abalin.net/docs/ 
 
 export class GetNameDay {
 
-    
+    constructor(name) {
+        this.name = name
+    }
+
+    async getNameDay() {
+        const url = new Url('https://nameday.abalin.net/api/V1/getname')
+
+        const params = {
+            "name": this.name,
+            "country": se
+        }
+
+        Object.keys(params)
+            .forEach(key => url.searchParams.append(key, params[key]))
+
+        const headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }
+
+        
+    }
 
 
 
