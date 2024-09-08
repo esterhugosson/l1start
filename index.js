f015c220bc86075a24f2eb3d177f84116e5a34ac
Ester Hugosson <eh225bx@student.lnu.se>
2024-09-08 17:36:41 +0200

    js file

---
+++ index.js
@@ -0,0 +1,11 @@
+
+/**
+ * Js file for L1 
+ * @author Ester Hugosson
+ */
+
+// Listening when a name is entered and then presenting the result
+
+document.addEventListener('DOMContentLoaded', () => {
+
+})
